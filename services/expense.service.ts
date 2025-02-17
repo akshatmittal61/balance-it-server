1ff372de02e31366444209721bc71cda4498e464
Akshat Mittal <akshatmittal2506@gmail.com>
2025-02-18 05:01:50 +0530

    feat: added create and update expense services

--- services/expense.service.ts
+++ services/expense.service.ts
@@ -1,19 +1,33 @@
 import { cache, getCacheKey } from "../cache";
 import { cacheParameter, HTTP } from "../constants";
 import { ApiError } from "../errors";
-import { expenseRepo, groupRepo, splitRepo } from "../repo";
-import { CreateModel, Expense, Split } from "../types";
+import { expenseRepo, groupRepo, memberRepo, splitRepo } from "../repo";
+import {
+	CreateModel,
+	Expense,
+	IExpense,
+	Split,
+	T_EXPENSE_TYPE,
+	UpdateQuery,
+} from "../types";
+import { UserService } from "./user.service";
 
 export class ExpenseService {
-	public static async getUserExpenses(userId: string) {
+	public static async getUserExpenses(
+		userId: string
+	): Promise<Array<IExpense>> {
 		const expenses = await expenseRepo.find({ author: userId });
 		return expenses || [];
 	}
-	public static async getGroupExpenses(groupId: string) {
+	public static async getGroupExpenses(
+		groupId: string
+	): Promise<Array<IExpense>> {
 		const expenses = await expenseRepo.find({ group: groupId });
 		return expenses || [];
 	}
-	public static async getExpenseById(expenseId: string) {
+	public static async getExpenseById(
+		expenseId: string
+	): Promise<IExpense | null> {
 		const cacheKey = getCacheKey(cacheParameter.EXPENSE, { id: expenseId });
 		return await cache.fetch(cacheKey, () =>
 			expenseRepo.findById(expenseId)
@@ -23,16 +37,76 @@ export class ExpenseService {
 		body: CreateModel<Expense>,
 		userId: string,
 		splits?: Array<{ user: string; amount: number }>
-	) {
-		if (body.author && body.author !== userId) {
-			throw new ApiError(HTTP.status.UNAUTHORIZED, "Unauthorized");
+	): Promise<IExpense> {
+		// --- Validations ---
+		const foundAuthor = await UserService.getUserById(body.author);
+		if (!foundAuthor) {
+			throw new ApiError(HTTP.status.NOT_FOUND, "User not found");
+		}
+		if (!body.group && !splits) {
+			if (body.author !== userId) {
+				// Only the current user can add a personal expense
+				// check if the user is the one adding the expense
+				throw new ApiError(HTTP.status.UNAUTHORIZED, "Unauthorized");
+			}
+		}
+		// check if the amount is valid
+		if (isNaN(body.amount) || body.amount <= 0) {
+			throw new ApiError(HTTP.status.BAD_REQUEST, "Invalid amount");
 		}
 		if (body.group) {
+			// check if the user is a member of the group
 			const foundGroup = await groupRepo.findById(body.group);
 			if (!foundGroup) {
 				throw new ApiError(HTTP.status.NOT_FOUND, "Group not found");
 			}
+			const members = await memberRepo.find({ group: foundGroup.id });
+			if (!members) {
+				throw new ApiError(HTTP.status.NOT_FOUND, "Group not found");
+			}
+			if (!members.some((member) => member.user.id === userId)) {
+				throw new ApiError(
+					HTTP.status.UNAUTHORIZED,
+					"You are not a member of this group"
+				);
+			}
+			// check if splits are not provided
+			if (!splits) {
+				throw new ApiError(
+					HTTP.status.BAD_REQUEST,
+					"Splits are required for group expenses"
+				);
+			}
 		}
+		if (splits) {
+			// check if the amount is distributed correctly
+			const distributedAmounts = splits.reduce((acc, split) => {
+				return acc + split.amount;
+			}, 0);
+			if (distributedAmounts !== body.amount) {
+				throw new ApiError(
+					HTTP.status.BAD_REQUEST,
+					"Invalid split amounts"
+				);
+			}
+			// check if the users exist
+			const users = splits.map((split) => split.user);
+			const foundMembers = await memberRepo.find({
+				user: { $in: users },
+			});
+			if (!foundMembers) {
+				throw new ApiError(HTTP.status.NOT_FOUND, "Members not found");
+			}
+			// check if everyone in the splits has some amount
+			if (splits.some((split) => split.amount <= 0)) {
+				throw new ApiError(
+					HTTP.status.BAD_REQUEST,
+					"Invalid split amounts"
+				);
+			}
+		}
+
+		// --- Create expense ---
 		const expense = await expenseRepo.create({ ...body, author: userId });
 		if (splits) {
 			const splitsToCreate: Array<CreateModel<Split>> = splits.map(
@@ -48,5 +122,179 @@ export class ExpenseService {
 			await splitRepo.bulkCreate(splitsToCreate);
 		}
 		return expense;
+	}
+	public static async updateExpense({
+		expenseId,
+		loggedInUserId,
+		title,
+		amount,
+		author,
+		timestamp,
+		group,
+		tags,
+		icon,
+		type,
+		method,
+		splits,
+	}: {
+		expenseId: string;
+		loggedInUserId: string;
+		title?: string;
+		amount?: number;
+		author?: string;
+		timestamp?: string;
+		group?: string;
+		tags?: string[];
+		icon?: string;
+		type?: T_EXPENSE_TYPE;
+		method?: string;
+		splits?: Array<{ user: string; amount: number }>;
+	}): Promise<IExpense> {
+		if (amount === null || amount === undefined || isNaN(amount)) {
+			throw new ApiError(HTTP.status.BAD_REQUEST, "Invalid amount");
+		}
+		if (splits) {
+			// check if the amount is distributed correctly
+			const distributedAmounts = splits.reduce((acc, split) => {
+				return acc + split.amount;
+			}, 0);
+			if (distributedAmounts !== amount) {
+				throw new ApiError(
+					HTTP.status.BAD_REQUEST,
+					"Invalid split amounts"
+				);
+			}
+			// check if the users exist
+			const users = splits.map((split) => split.user);
+			const foundMembers = await memberRepo.find({
+				user: { $in: users },
+			});
+			if (!foundMembers) {
+				throw new ApiError(HTTP.status.NOT_FOUND, "Members not found");
+			}
+			// check if everyone in the splits has some amount
+			if (splits.some((split) => split.amount <= 0)) {
+				throw new ApiError(
+					HTTP.status.BAD_REQUEST,
+					"Invalid split amounts"
+				);
+			}
+		}
+		const foundExpense = await ExpenseService.getExpenseById(expenseId);
+		if (!foundExpense) {
+			throw new ApiError(HTTP.status.NOT_FOUND, "Expense not found");
+		}
+		// the user can only edit expense if it is paid by the user or is a personal expense
+		if (foundExpense.author.id !== loggedInUserId) {
+			throw new ApiError(HTTP.status.UNAUTHORIZED, "Unauthorized");
+		}
+		if (splits) {
+			const existingSplits =
+				(await splitRepo.find({
+					expense: expenseId,
+				})) || [];
+			const splitsToCreate: Array<CreateModel<Split>> = splits
+				.filter((split) => {
+					const existingSplit = existingSplits.find(
+						(s) => s.user.id === split.user
+					);
+					return !existingSplit;
+				})
+				.map((split) => ({
+					expense: expenseId,
+					user: split.user,
+					pending:
+						foundExpense.author.id === split.user
+							? 0
+							: split.amount,
+					completed:
+						foundExpense.author.id === split.user
+							? split.amount
+							: 0,
+				}));
+			const splitsToUpdate: Array<UpdateQuery<Split>> = splits
+				.filter((split) => {
+					const existingSplit = existingSplits.find(
+						(s) => s.user.id === split.user
+					);
+					return existingSplit;
+				})
+				.map((split) => ({
+					filter: {
+						_id: existingSplits.find(
+							(s) => s.user.id === split.user
+						)!.id,
+					},
+					update: {
+						$set: {
+							pending:
+								foundExpense.author.id === split.user
+									? 0
+									: split.amount,
+							completed:
+								foundExpense.author.id === split.user
+									? split.amount
+									: 0,
+						},
+					},
+				}));
+			const splitsToDelete: Array<string> = existingSplits
+				.filter((split) => {
+					const existingSplit = splits.find(
+						(s) => s.user === split.user.id
+					);
+					return existingSplit === undefined;
+				})
+				.map((split) => split.id);
+			await splitRepo.bulkCreate(splitsToCreate);
+			await splitRepo.bulkUpdate(splitsToUpdate);
+			await splitRepo.bulkRemove({ _id: { $in: splitsToDelete } });
+		}
+		if (group) {
+			// check if the group exists
+			const foundGroup = await groupRepo.findOne({ id: group });
+			if (!foundGroup) {
+				throw new ApiError(HTTP.status.NOT_FOUND, "Group not found");
+			}
+			// check if the user is a member of the group
+			const members = await memberRepo.find({ group: foundGroup.id });
+			if (!members) {
+				throw new ApiError(HTTP.status.NOT_FOUND, "Group not found");
+			}
+			if (!members.some((member) => member.user.id === loggedInUserId)) {
+				throw new ApiError(
+					HTTP.status.UNAUTHORIZED,
+					"You are not a member of this group"
+				);
+			}
+			// check if the person who paid for this expense is a member of the group
+			if (members.some((member) => member.user.id === author)) {
+				throw new ApiError(
+					HTTP.status.UNAUTHORIZED,
+					"You are not a member of this group"
+				);
+			}
+		}
+		const updateExpenseBody: UpdateQuery<IExpense> = {
+			$set: {
+				title,
+				amount,
+				author,
+				timestamp,
+				group,
+				tags,
+				icon,
+				type,
+				method,
+			},
+		};
+		const updatedExpense = await expenseRepo.update(
+			{ id: expenseId },
+			updateExpenseBody
+		);
+		if (!updatedExpense) {
+			throw new ApiError(HTTP.status.NOT_FOUND, "Expense not found");
+		}
+		return updatedExpense;
 	}
 }
--- types/models.ts
+++ types/models.ts
@@ -6,6 +6,14 @@ import {
 } from "./enum";
 import { Model } from "./parser";
 
+/**
+ * AuthMapping model
+ * @param {string} identifier - Identifier of the user
+ * @param {string} providerId - Provider id of auth service
+ * @param {string} providerName - Provider name of auth service
+ * @param {Object} misc - Misc data of the user (optional)
+ * @param {string} user - User id (References User model) (optional - for non-onboarded users)
+ */
 export type AuthMapping = Model<{
 	identifier: string;
 	providerId: string;
@@ -13,6 +21,16 @@ export type AuthMapping = Model<{
 	misc?: any;
 	user: string | null;
 }>;
+
+/**
+ * User model
+ * @param {string} name - Name of the user (optional - defaults to email prefix)
+ * @param {string} email - Email of the user
+ * @param {string} phone - Phone number of the user (optional)
+ * @param {string} avatar - Avatar of the user (optional)
+ * @param {string} status - Status of the user (Joined, Invited)
+ * @param {string} invitedBy - Id of the user who invited the user (References User model) (optional - for invited users)
+ */
 export type User = Model<{
 	name?: string;
 	email: string;
@@ -22,6 +40,18 @@ export type User = Model<{
 	invitedBy?: string;
 }>;
 
+/**
+ * Expense model
+ * @param {string} title - Title of the expense
+ * @param {number} amount - Amount of the expense
+ * @param {string} author - Author of the expense (References User model)
+ * @param {string} timestamp - Creation time of the expense
+ * @param {string} group - Group id (References Group model) (optional - for group expenses)
+ * @param {string[]} tags - Tags for the expense (optional)
+ * @param {string} icon - Icon of the expense (optional)
+ * @param {string} type - Type of the expense (Personal, Group)
+ * @param {string} method - Method of the expense (Cash, Card, UPI, Netbanking)
+ */
 export type Expense = Model<{
 	title: string;
 	amount: number;
@@ -34,6 +64,13 @@ export type Expense = Model<{
 	method?: string;
 }>;
 
+/**
+ * Split model - When a user splits an expense
+ * @param {string} expense - Expense id (References Expense model)
+ * @param {string} user - User id (References User model)
+ * @param {number} pending - Pending amount
+ * @param {number} completed - Completed amount
+ */
 export type Split = Model<{
 	expense: string;
 	user: string;
@@ -41,6 +78,14 @@ export type Split = Model<{
 	completed: number;
 }>;
 
+/**
+ * Group model
+ * @param {string} name - Name of the group
+ * @param {string} icon - Icon of the group (optional)
+ * @param {string} banner - Banner of the group (optional)
+ * @param {string[]} tags - Tags of the group
+ * @param {string} author - Author of the group (References User model)
+ */
 export type Group = Model<{
 	name: string;
 	icon?: string;
@@ -49,6 +94,13 @@ export type Group = Model<{
 	author: string;
 }>;
 
+/**
+ * Member model - Group Member entity
+ * @param {string} user - User id (References User model)
+ * @param {string} group - Group id (References Group model)
+ * @param {string} status - Status of the member (Joined, Left, Invited, Pending)
+ * @param {string} role - Role of the member (Owner, Admin, Member)
+ */
 export type Member = Model<{
 	user: string;
 	group: string;
